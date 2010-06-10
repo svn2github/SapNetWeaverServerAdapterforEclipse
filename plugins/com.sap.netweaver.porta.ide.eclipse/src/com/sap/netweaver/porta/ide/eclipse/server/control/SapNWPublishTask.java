6855a8201e4fc7699755009dfa19a96f068b4c8b
kaloyan_raev <kaloyan_raev@33308a02-c225-4ae4-b9a8-909b9aa047d5>
2010-06-10 09:49:43 +0000

    #10 - Publish not executed when changes done in submodules of EAR
    git-svn-id: https://svn.code.sf.net/p/sapnweclipse/code/trunk@68 33308a02-c225-4ae4-b9a8-909b9aa047d5

--- plugins/com.sap.netweaver.porta.ide.eclipse/src/com/sap/netweaver/porta/ide/eclipse/server/control/SapNWPublishTask.java
+++ plugins/com.sap.netweaver.porta.ide.eclipse/src/com/sap/netweaver/porta/ide/eclipse/server/control/SapNWPublishTask.java
@@ -10,8 +10,9 @@
  *******************************************************************************/
 package com.sap.netweaver.porta.ide.eclipse.server.control;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
@@ -28,20 +29,39 @@ public class SapNWPublishTask extends PublishTaskDelegate {
 			return null;
 		
 		SapNWServerBehavior serverControl = (SapNWServerBehavior) server.loadAdapter(SapNWServerBehavior.class, null);
-		
-		List<PublishOperation> tasks = new ArrayList<PublishOperation>();
+
+		// find all root modules that needs to be deployed
+		Map<IModule, Integer> rootModules = new HashMap<IModule, Integer>();
 		int size = modules.size();
 		for (int i = 0; i < size; i++) {
 			IModule[] module = (IModule[]) modules.get(i);
 			Integer deltaKind = (Integer) kindList.get(i);
-			if (module.length == 1 
-					&& deltaKind != ServerBehaviourDelegate.NO_CHANGE 
+			
+			// check if the module is modified and its root module is deployable
+			if (deltaKind != ServerBehaviourDelegate.NO_CHANGE 
 					&& SapNWServerUtil.isDeployableModule(module[0])) {
-				tasks.add(new SapNWPublishOperation(serverControl, kind, module, deltaKind));
+				if (module.length == 1) {
+					// if root module then add it to the task list
+					rootModules.put(module[0], deltaKind);
+				} else {
+					// if submodule, then check if its root module is already added to the task list
+					// if not, then add it to the task list. 
+					if (!rootModules.containsKey(module[0])) {
+						rootModules.put(module[0], ServerBehaviourDelegate.CHANGED);
+					}
+				}
 			}
 		}
 		
-		return (PublishOperation[]) tasks.toArray(new PublishOperation[tasks.size()]);
+		// construct publish operations
+		PublishOperation[] operations = new PublishOperation[rootModules.size()];
+		int i = 0;
+		for (IModule module : rootModules.keySet()) {
+			int deltaKind = rootModules.get(module);
+			operations[i++] = new SapNWPublishOperation(serverControl, kind, new IModule[] { module }, deltaKind);
+		}
+		
+		return operations;
 	}
-
+	
 }
